8d614974c98d0039ced025ff08809af4dddbf3e1
Nyasha (Nash) Nziramasanga <47994666+NyashaNziramasanga@users.noreply.github.com>
2020-01-17 16:16:22 +1100

    Test example

---
+++ maths.js
@@ -0,0 +1,14 @@
+// Addition and Subtraction test
+
+const maths ={
+    
+    sum: (a,b) =>{
+        return a+ b;
+    },
+
+    subtract: (a,b) =>{
+        return a - b;
+    }
+}
+
+module.exports = maths; 
